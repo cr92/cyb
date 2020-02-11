61c98bc3efb413ab6d772f1a75fe69fe8f9bc2fa
Agni Roy <a.roy@f5.com>
2020-02-11 17:10:39 +0530

    item discount validation

--- app/controllers/item-discount-controller.js
+++ app/controllers/item-discount-controller.js
@@ -1,11 +1,24 @@
 'use strict';
 
 const itemDiscount = require('../models/item-discount.js');
+const item=require('../models/item.js');
 
 module.exports = {
 
-    addItemDiscount: (req, res, next) => {
+    addItemDiscount: async (req, res, next) => {
         const itemDiscountInfo = req.body;
+        const reqItemsList = itemDiscountInfo.appliesTo || [];
+        let validItemsList = [];
+        try {
+            validItemsList = (await Promise.all(reqItemsList.map(itemId => item.findById(itemId)))).filter(Boolean);
+        } catch (e) {
+            return next(e);
+        }
+
+        if (validItemsList.length !== reqItemsList.length) {
+            return next(new Error('Cannot apply discount on invalid items'))
+        }
+
         itemDiscount.create(itemDiscountInfo)
             .then((data) => res.send(data))
             .catch(next);
